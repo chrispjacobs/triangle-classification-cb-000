6229928378a9f1a8e073bec1b9acda45750491d4
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-07 01:00:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,5 +10,20 @@ class Triangle
       @side_one = side_one
       @side_two = side_two
       @side_three = side_three
-
+      @kind = nil
+    end
+  end
+  def special_triangle_check
+    if @side_one == @side_two && @side_one != @side_three
+      @kind = :isosceles
+    elsif @side_one == @side_three && @side_one != @side_two
+      @kind = :isosceles
+    elsif @side_two == @side_three && @side_one != @side_two
+      @kind = :isosceles
+    elsif @side_one == @side_two && @side_two == @side_three
+      @kind = :equilateral
+    else
+      @kind = :scalene
+    end
+  end
 end
