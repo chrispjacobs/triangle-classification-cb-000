97ff8952c2f83a2fb1cede08e900c8a3c42e16fd
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-07 01:03:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,7 @@ class Triangle
       rescue TriangleError => error
         puts error.message
       end
+    elsif side_one <= 0 || side_two <= 0 || side_three <=0
     else
       @side_one = side_one
       @side_two = side_two
